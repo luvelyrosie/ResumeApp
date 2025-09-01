dd0f32790d46e099e859561bc3cdb318bbbc16f1
luvelyrosie <arzygulaagajanowa@gmail.com>
2025-09-01 22:12:32 +0500

    fixed a database url for railway

--- backend/app/database.py
+++ backend/app/database.py
@@ -12,17 +12,12 @@ def is_docker() -> bool:
     except socket.gaierror:
         return False
 
-# if is_docker():
-#     DATABASE_URL = "postgresql://postgres:luvelyrosie7@db:5432/ResumeAppDatabase"
-# else:
-#     DATABASE_URL = "postgresql://postgres:luvelyrosie7@localhost:5432/ResumeAppDatabase"
 
-if is_docker():
-    DATABASE_URL = "postgresql://postgres:postgres123@db:5432/ResumeAppDatabase"
-else:
-    DATABASE_URL = "postgresql://postgres:postgres123@localhost:5432/ResumeAppDatabase"
-
-# print(f"Using database URL: {DATABASE_URL}")
+DATABASE_URL = os.environ.get("DATABASE_URL") or (
+    "postgresql://postgres:postgres123@db:5432/ResumeAppDatabase"
+    if is_docker()
+    else "postgresql://postgres:postgres123@localhost:5432/ResumeAppDatabase"
+)
 
 
 engine=create_engine(DATABASE_URL)
